ffa723d6f45a0a96d1f87c8af4b9d9ab5a17af34
xuhaifu <xuhaifu@banggood.com>
2018-05-02 18:18:44 +0800

    test

--- client.php
+++ client.php
@@ -1,7 +1,32 @@
 <?php
 
-/******************连接tcp_server的客户端************************************************/
+/******************异步TCP客户端************************************************/
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+//连接到服务器
+if (!$client->connect('127.0.0.1', 9501, 0.5))
+{
+    die("connect failed.");
+}
+//向服务器发送数据
+if (!$client->send("hello world"))
+{
+    die("send failed.");
+}
+//从服务器接收数据
+$data = $client->recv();
+if (!$data)
+{
+    die("recv failed.");
+}
+echo $data;
+//关闭连接
+$client->close();
 
+
+
+
+/******************同步TCP客户端************************************************/
+exit;
 $client = new swoole_client(SWOOLE_SOCK_TCP);
 //连接到服务器
 if (!$client->connect('127.0.0.1', 9501, 0.5))
--- tcp_server.php
+++ tcp_server.php
@@ -6,7 +6,29 @@
  * Time: 14:53
  */
 
-/*******************创建TCP服务器************************************************/
+/*******************通过TCP连接执行异步任务************************************************/
+$serv = new swoole_server("127.0.0.1", 9501);
+//设置异步任务的工作进程数量
+$serv->set(array('task_worker_num' => 2));
+$serv->on('receive', function($serv, $fd, $from_id, $data) {
+    //投递异步任务
+    $task_id = $serv->task($data);
+    echo "Dispath AsyncTask: id=$task_id\n";
+});
+//处理异步任务
+$serv->on('task', function ($serv, $task_id, $from_id, $data) {
+    echo "New AsyncTask[id=$task_id]".PHP_EOL;
+    //返回任务执行的结果
+    $serv->finish("$data: -> OK");
+});
+//处理异步任务的结果
+$serv->on('finish', function ($serv, $task_id, $data) {
+    echo "AsyncTask[$task_id] Finish: $data".PHP_EOL;
+});
+$serv->start();
+
+/*******************创建同步TCP服务器************************************************/
+exit;
 //创建Server对象，监听 127.0.0.1:9501端口
 $serv = new swoole_server("127.0.0.1", 9501);
 //var_dump($serv);
